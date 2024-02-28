b6679c94fb48e863f8d79eaba7a0bce9d2ef3794
Andylaa10 <andylaa100@gmail.com>
2024-02-28 16:50:34 +0100

    Update UserController.cs

--- TwitterMock/UserService/Controllers/UserController.cs
+++ TwitterMock/UserService/Controllers/UserController.cs
@@ -6,4 +6,5 @@ namespace UserService.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
+    // https://dev.to/behalf/authentication-authorization-in-microservices-architecture-part-i-2cn0
 }

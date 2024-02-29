6dad68fb8ad56a317a449a64ce7d82a8639a2e92
Andylaa10 <andylaa100@gmail.com>
2024-02-29 11:11:01 +0100

    Update UserController.cs

--- TwitterMock/UserService/Controllers/UserController.cs
+++ TwitterMock/UserService/Controllers/UserController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace UserService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class UserController : ControllerBase
 {
     // https://dev.to/behalf/authentication-authorization-in-microservices-architecture-part-i-2cn0
+    // https://www.linkedin.com/pulse/api-gateways-cnet-amir-doosti/
+    // https://ocelot.readthedocs.io/en/latest/features/authentication.html 
 }
